1c66fc00258bec4e9c010074d946196c831a0f7d
ChaitanyaThota <chaitanyathota123@gmail.com>
2019-09-15 13:26:52 +0530

    updated HomePage headervalue

--- src/main/java/com/qa/hubspot/util/Constants.java
+++ src/main/java/com/qa/hubspot/util/Constants.java
@@ -2,8 +2,8 @@ package com.qa.hubspot.util;
 
 public class Constants {
 	public final static String LOGIN_PAGE_TITLE = "HubSpot Login";
-	public final static String HOME_PAGE_TITLE = "Getting Started | HubSpot";
-	public final static String HOMEPAGE_HEADER_VALUE = "Getting started with HubSpot";
+	public final static String HOME_PAGE_TITLE = "Account Setup | HubSpot";
+	public final static String HOMEPAGE_HEADER_VALUE = "Set up your HubSpot account";
 	public final static String CONTACTS_PAGE_TITLE = "Contacts";
 
 }
