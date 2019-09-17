4cb0e2fdee2214fd4172371a5a22ee7eedff6782
ChaitanyaThota <chaitanyathota123@gmail.com>
2019-09-17 13:28:34 +0530

    created Deals Page Code

--- src/main/java/com/qa/hubspot/pages/DealsPage.java
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -1,13 +1,14 @@
 package com.qa.hubspot.pages;
+
 import org.openqa.selenium.WebDriver;
+
 import com.qa.hubspot.Base.BasePage;
-public class DealsPage extends BasePage{
+
+public class DealsPage extends BasePage {
+	WebDriver driver;
+	public void SalesPageCode(WebDriver driver) {
+		this.driver = driver;
+	}
 	
-public DealsPage(WebDriver driver) {
-	this.driver=driver;
-	
-	
-	
-}
 
 }
