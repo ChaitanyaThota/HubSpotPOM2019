7bb10f4894d054354404a0d7efe1c0ddfa9016a6
chaitanyathota <chaitanyathota123@gmail.com>
2019-09-17 22:32:58 +0530

    added comments

--- src/main/java/com/qa/hubspot/pages/DealsPage.java
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -6,6 +6,8 @@ import com.qa.hubspot.Base.BasePage;
 
 public class DealsPage extends BasePage {
 	WebDriver driver;
+	
+	// contructor for this page
 	public void SalesPageCode(WebDriver driver) {
 		this.driver = driver;
 	}
